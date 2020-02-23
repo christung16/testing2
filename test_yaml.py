0697e686e6f0a107f5c87ea41a33bf7b4e712846
christung16 <yitung@cisco.com>
2020-02-23 23:56:23 +0800

    change

---
+++ test_yaml.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import yaml
+import json
+import xml.etree.ElementTree as ET
+import dicttoxml
+import xml.dom.minidom
+import xmltodict
+
+with open('test-playbook.yml', 'r') as file:
+    data = yaml.safe_load(file)
+user = data[0]['vars']
+print (user)
+file.close()
+
+with open('test-playbook-var.yml','w') as file:
+    yaml.dump(user, file, default_flow_style=False)
+file.close()
+
+with open('test-playbook.json', 'w') as file:
+    json.dump(data, file, indent=4)
+file.close()
+
+with open('test-playbook.json', 'r') as file:
+    json_data = json.load(file)
+file.close()
+
+xml_object = dicttoxml.dicttoxml(data)
+
+with open('test-playbook.xml', 'w') as file:
+    file.write(xml_object.decode())
+file.close()
+
+with open('test-playbook.xml', 'r') as file:
+    myroot=xml.dom.minidom.parse(file)
+file.close()
+
+prettyxml=myroot.toprettyxml()
+print (prettyxml)
+todict=xmltodict.parse(prettyxml)
+print (json.dumps(todict,indent=4))
+
+
+print (todict)
+
+
+
+
+
--- testing2.py
+++ testing2.py
@@ -5,8 +5,10 @@
 import xml.dom.minidom
 
 xmlString = """<?xml version="1.0" encoding="UTF-8"?><server><description>My virtual server test</description><destination>1.1.1.3:80</destination><ipProtocol>tcp</ipProtocol><kind>tm:ltm:virtual:virtualstate</kind><mask>255.255.255.255</mask><name>http-virtual</name><pool>http</pool><profiles><element><kind>ltm:virtual:profile</kind><name>http</name></element><element><kind>ltm:virtual:profile</kind><name>tcp</name></element></profiles><sourceAddressTranslation><type>automap</type></sourceAddressTranslation></server>"""
+a = "1"
 
 dom = xml.dom.minidom.parseString(xmlString)
 prettyxml = dom.toprettyxml()
 print (prettyxml)
+
 
